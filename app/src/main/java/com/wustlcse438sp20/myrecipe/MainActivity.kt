ce32336cff0dcaa0386eb7ce84b6b5e703fe8c4e
Haoyu Wu <37765853+allrightteam@users.noreply.github.com>
2020-04-04 14:53:28 -0500

    Add files via upload

---
+++ app/src/main/java/com/wustlcse438sp20/myrecipe/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wustlcse438sp20.myrecipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
