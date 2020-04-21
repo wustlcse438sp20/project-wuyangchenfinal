73a327d637fa8d2a66002de96d8cf6c259551e93
Hunnyishere <yangruoyao@wustl.edu>
2020-04-20 20:02:46 -0500

    comment

--- app/src/main/java/com/wustlcse438sp20/myrecipe/Adapter/ItemTouchHelperAdapter.kt
+++ app/src/main/java/com/wustlcse438sp20/myrecipe/Adapter/ItemTouchHelperAdapter.kt
@@ -10,33 +10,5 @@ import com.wustlcse438sp20.myrecipe.data.RecipeShownFormat
 
 interface ItemTouchHelperAdapter {
 
-   fun onItemDissmiss(position:Int){
-      // remove from database
-//      db.collection("collections")
-//         .whereEqualTo(FieldPath.documentId(), collection_id)
-//         .get()
-//         .addOnCompleteListener(OnCompleteListener<QuerySnapshot> { task ->
-//            if (task.isSuccessful) {
-//               Log.v("Search in database", "Sucess")
-//               println("search success !!!!!!!!!!!!!!!!!!")
-//               for (document in task.result!!) {
-//
-//                  var recipes = document.get("recipes") as ArrayList<MutableMap<String,Any>>){
-//                     var recipe_id:Int =recipe["id"].toString().toInt()
-//                     var recipe_title:String = recipe["title"].toString()
-//                     var recipe_image:String = recipe["image"].toString()
-//                     Log.v("recipe id",recipe_id.toString())
-//                     Log.v("recipe title",recipe_title)
-//                     Log.v("recipe image",recipe_image)
-//                     recipeList.add(RecipeShownFormat(recipe_id,recipe_title,recipe_image))
-//                  }
-//               }
-//               adapter.notifyDataSetChanged()
-//            }
-//            else {
-//               Log.v("Search in database", "Fail")
-//               println("failed to get user data")
-//            }
-//         })
-   }
+   fun onItemDissmiss(position:Int)
 }
